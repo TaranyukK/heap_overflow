45df8241449dba63f122abe9860ef3e30b024a01
TaranyukK <taranyuk.kirill@gmail.com>
2024-10-11 00:08:32 +0300

    feat: add url validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ApplicationRecord
+  VALID_URL_REGEX = /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/i
+
   belongs_to :linkable, polymorphic: true
 
-  validates :name, :url, presence: true
+  validates :url, presence: true, format: { with: VALID_URL_REGEX }
+  validates :name, presence: true
 end
