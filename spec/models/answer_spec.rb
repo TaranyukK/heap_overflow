a9fbe20ba6a8e7ba6a5512ce63dc9ce7694db997
Kirill Taranyuk <taranyuk.kirill@gmail.com>
2024-10-29 00:44:25 +0300

    create linkable concern

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should have_many(:links).dependent(:destroy) }
+  it_behaves_like 'linkable'
 
   it { should belong_to :question }
 
   it { should validate_presence_of :body }
-
-  it { should accept_nested_attributes_for :links }
 
   describe 'scopes' do
     let!(:answers) { create_list(:answer, 3) }
