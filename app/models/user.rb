e6ada08dcd04eb3c901e2f74b2889ce97254cf47
Kirill Taranyuk <taranyuk.kirill@gmail.com>
2024-11-18 02:56:34 +0300

    feat: move logic from model to service; refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,23 +15,6 @@ class User < ApplicationRecord
   end
 
   def self.find_for_oauth(auth)
-    authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)
-
-    return authorization.user if authorization
-
-    email = auth.info.email
-    user = User.find_by(email:)
-
-    unless user
-      password = Devise.friendly_token[0, 20]
-      user = User.create!(email:, password:, password_confirmation: password)
-    end
-    user.create_authorization(auth)
-
-    user
-  end
-
-  def create_authorization(auth)
-    authorizations.create(provider: auth.provider, uid: auth.uid)
+    FindForOauthService.new(auth).call
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,74 +18,15 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe '.find_for_oauth' do
+  describe '.find_for_auth' do
     let!(:user) { create(:user) }
     let(:auth) { OmniAuth::AuthHash.new(provider: 'github', uid: '123456') }
+    let(:service) { instance_double(FindForOauthService) }
 
-    context 'user already has authorization' do
-      it 'returns user' do
-        user.authorizations.create(provider: 'github', uid: '123456')
-
-        expect(described_class.find_for_oauth(auth)).to eq user
-      end
-    end
-
-    context 'user has no authorization' do
-      context 'user already exists' do
-        let(:auth) { OmniAuth::AuthHash.new(provider: 'github', uid: '123456', info: { email: user.email }) }
-
-        it 'does not create new user' do
-          expect { described_class.find_for_oauth(auth) }.not_to change(described_class, :count)
-        end
-
-        it 'creates new authorization for user' do
-          expect { described_class.find_for_oauth(auth) }.to change(user.authorizations, :count).by(1)
-        end
-
-        it 'creates new authorization with provider and uid' do
-          authorization = described_class.find_for_oauth(auth).authorizations.first
-
-          expect(authorization.provider).to eq auth.provider
-          expect(authorization.uid).to eq auth.uid
-        end
-
-        it 'returns user' do
-          expect(described_class.find_for_oauth(auth)).to eq user
-        end
-      end
-
-      context 'user does not exist' do
-        let(:auth) do
-          OmniAuth::AuthHash.new(provider: 'github', uid: '123456', info: { email: 'new_user@example.com' })
-        end
-
-        it 'creates new user' do
-          expect { described_class.find_for_oauth(auth) }.to change(described_class, :count).by(1)
-        end
-
-        it 'returns user' do
-          expect(described_class.find_for_oauth(auth)).to be_a described_class
-        end
-
-        it 'fills user email' do
-          user = described_class.find_for_oauth(auth)
-
-          expect(user.email).to eq 'new_user@example.com'
-        end
-
-        it 'creates authorization for user' do
-          user = described_class.find_for_oauth(auth)
-
-          expect(user.authorizations).not_to be_empty
-        end
-
-        it 'creates new authorization with provider and uid' do
-          authorization = described_class.find_for_oauth(auth).authorizations.first
-
-          expect(authorization.provider).to eq auth.provider
-          expect(authorization.uid).to eq auth.uid
-        end
-      end
+    it 'calls FindForOauthService' do
+      expect(FindForOauthService).to receive(:new).with(auth).and_return(service)
+      expect(service).to receive(:call)
+      described_class.find_for_oauth(auth)
     end
   end
 end
