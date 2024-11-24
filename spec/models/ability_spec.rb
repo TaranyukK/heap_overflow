d543262ae5983ba5492f27b100d5fa3eb2003054
Kirill Taranyuk <taranyuk.kirill@gmail.com>
2024-11-24 15:55:41 +0300

    add more abilities

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -13,12 +13,14 @@ RSpec.describe Ability do
   describe 'for user' do
     let(:user) { create(:user) }
     let(:other_user) { create(:user) }
-    let(:question) { create(:question, user: user) }
+    let(:question) { create(:question, :with_file, user: user) }
     let(:answer) { create(:answer, question: question, user: user) }
     let(:comment) { create(:comment, commentable: question, user: user) }
-    let(:other_question) { create(:question, user: other_user) }
+    let(:other_question) { create(:question, :with_file, user: other_user) }
     let(:other_answer) { create(:answer, question: other_question, user: other_user) }
     let(:other_comment) { create(:comment, commentable: other_question, user: other_user) }
+    let(:link) { create(:link, linkable: question) }
+    let(:other_link) { create(:link, linkable: other_question) }
 
     it { should be_able_to :read, :all }
     it { should_not be_able_to :manage, :all }
@@ -44,6 +46,27 @@ RSpec.describe Ability do
 
     it { should be_able_to :destroy, comment }
     it { should_not be_able_to :destroy, other_comment }
+
+    it { should be_able_to :vote_up, other_question }
+    it { should_not be_able_to :vote_up, question }
+
+    it { should be_able_to :vote_down, other_question }
+    it { should_not be_able_to :vote_down, question }
+
+    it { should be_able_to :vote_up, other_answer }
+    it { should_not be_able_to :vote_up, answer }
+
+    it { should be_able_to :vote_down, other_answer }
+    it { should_not be_able_to :vote_down, answer }
+
+    it { should be_able_to :mark_as_best, create(:answer, question: question) }
+    it { should_not be_able_to :mark_as_best, create(:answer, question: other_question) }
+
+    it { should be_able_to :destroy, link }
+    it { should_not be_able_to :destroy, other_link }
+
+    it { should be_able_to :destroy, question.files.first }
+    it { should_not be_able_to :destroy, other_question.files.first }
   end
 
   describe 'for admin' do
