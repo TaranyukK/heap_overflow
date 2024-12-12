b0ee8313229c275c1cb330041be69aa0505be8b6
Kirill Taranyuk <taranyuk.kirill@gmail.com>
2024-12-12 23:05:49 +0300

    feat: edit config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,11 @@
 lock "~> 3.19.2"
 
 set :application, "heap_overflow"
-set :repo_url, "git@github.com:TaranyukK/heap_overflow.git"
+set :repo_url,    "git@github.com:TaranyukK/heap_overflow.git"
+set :branch,      fetch(:branch, 'master')
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/heap_overflow'
+set :deploy_to,   '/home/deploy/heap_overflow'
 set :deploy_user, 'deploy'
 
 # Default value for :linked_files is []
