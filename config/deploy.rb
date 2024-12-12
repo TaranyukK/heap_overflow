a52c53e5b0b6fb913ebb6e3cf63298f1df843337
Kirill Taranyuk <taranyuk.kirill@gmail.com>
2024-12-13 02:20:48 +0300

    refactor: return newest sidekiq

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.19.2"
 set :application, "heap_overflow"
 set :repo_url,    "git@github.com:TaranyukK/heap_overflow.git"
 set :branch,      fetch(:branch, 'master')
-
+set :pty, false
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to,   '/home/deploy/heap_overflow'
 set :deploy_user, 'deploy'
